27c82d0c563a5ed949da00507871a2b1c27df28d
JaganNatarajan <Jagan.Natarajan@Gmail.com>
2022-09-01 18:09:42 +0100

    Update LGICDB01.cbl

--- cics-genapp/base/src/COBOL/LGICDB01.cbl
+++ cics-genapp/base/src/COBOL/LGICDB01.cbl
@@ -18,7 +18,7 @@
       * Select customer details from DB2 table                         *
       *                                                                *
       * TEST: 02                                                       *
-      *                                                                *
+      *   jagan Testing                                                             *
       ******************************************************************
        IDENTIFICATION DIVISION.
        PROGRAM-ID. LGICDB01.
